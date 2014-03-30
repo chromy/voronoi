c41ff3fa87c2c8875e555e20b94cd4556e40b48a
Hector Dearman <hector.dearman@gmail.com>
2014-03-30 14:28:28 +0100

    updated example

--- example/demo.dart
+++ example/demo.dart
@@ -1,29 +1,33 @@
-/*
- * 
- */
-
 library demo;
 
-import 'dart:async';
 import 'dart:html';
 import 'dart:math';
 
 import 'package:voronoi/voronoi.dart';
 
-void main() {
-  print("Starting...");
-  CanvasElement canvas = querySelector("#area");
-  scheduleMicrotask(new VoronoiDemo(canvas).start);
-}
-
-Element notes = querySelector("#fps");
+final InputElement slider = querySelector("#slider");
+final InputElement button = querySelector("#button");
+final CanvasElement canvas = querySelector("#area");
+final Element notes = querySelector("#notes");
+final VoronoiDemo demo = new VoronoiDemo(canvas, 10, (new Random()).nextInt(100000));
 num fpsAverage;
 
-/// Display the animation's FPS in a div.
-void showFps(num fps) {
-  if (fpsAverage == null) fpsAverage = fps;
-  fpsAverage = fps * 0.05 + fpsAverage * 0.95;
-  notes.text = "${fpsAverage.round()} fps";
+void main() {
+  demo.start();
+  slider.onChange.listen((e) => update());
+  button.onClick.listen((e) => randomiseSeed());
+  update();
+}
+
+void update() {
+  demo.sites = int.parse(slider.value);
+  demo.recompute();
+  notes.text = "${demo.sites} sites";
+}
+
+void randomiseSeed() {
+  demo.seed = (new Random()).nextInt(100000);
+  update();
 }
 
 class VoronoiDemo {
@@ -31,58 +35,57 @@ class VoronoiDemo {
 
   num width;
   num height;
+  int sites;
+  int seed;
 
   Voronoi voronoi;
 
   num renderTime;
 
-  VoronoiDemo(this.canvas);
+  VoronoiDemo(this.canvas, this.sites, this.seed);
 
-  // Initialize the diagram and start the simulation.
+  // Initialize the diagram.
   void start() {
+
     // Measure the canvas element.
     Rectangle rect = canvas.parent.client;
     width = rect.width;
     height = rect.height;
     canvas.width = width;
-    
-    // Create random points.
-    var rng = new Random();
-    Point randomPoint() => new Point(rng.nextInt(width), rng.nextInt(height));
-    var randomPoints = new List.generate(400, (i) => randomPoint());
-    var randomUniquePoints = new Set.from(randomPoints);
-    
-    // Compute the diagram.
-    voronoi = new Voronoi(new List.from(randomUniquePoints), null, new Rectangle(0, 0, width, height));
-    
+
+    // Compute diagram.
+    recompute();
+
     // Draw diagram.
     requestRedraw();
   }
 
-  void draw(num _) {
-    // Update FPS
-    num time = new DateTime.now().millisecondsSinceEpoch;
-    if (renderTime != null) showFps(1000 / (time - renderTime));
-    renderTime = time;
+  void recompute() {
+    // Create random points.
+    var rng = new Random(seed);
+    Point randomPoint() => new Point(rng.nextInt(width), rng.nextInt(height));
+    var randomPoints = new List.generate(sites, (i) => randomPoint());
+    var randomUniquePoints = new Set.from(randomPoints);
 
-    // Draw 
+    // Compute the diagram.
+    voronoi = new Voronoi(new List.from(randomUniquePoints), null, new Rectangle(0, 0, width, height));
+  }
+
+  void draw(num _) {
+    // Draw
     var context = canvas.context2D;
     drawBackground(context);
     drawLines(context);
     drawSites(context);
     requestRedraw();
   }
-  
-  /**
-   * Clear the background to white.
-   */
+
+  /// Clear the background to white.
   void drawBackground(CanvasRenderingContext2D context) {
     context.clearRect(0, 0, width, height);
   }
 
-  /**
-   * Draw the sites of the cells on context.
-   */
+  /// Draw the sites of the cells on context.
   void drawSites(CanvasRenderingContext2D context) {
     for (var site in voronoi.siteCoords()) {
       context
@@ -93,15 +96,13 @@ class VoronoiDemo {
         ..fill();
     }
   }
-  
-  /**
-   * Draw the edges of the cells on context.
-   */
+
+  /// Draw the edges of the cells on context.
   void drawLines(CanvasRenderingContext2D context) {
 
     // Don't consider edges which have been clipped completely away.
     var edges = voronoi.edges.where((x) => x.visible);
-    
+
     for (var edge in edges) {
       // Create gradient
       var lingrad = context.createLinearGradient(
@@ -112,7 +113,7 @@ class VoronoiDemo {
       lingrad
         ..addColorStop(0, '#f00')
         ..addColorStop(1, '#0f0');
-        
+
       context
         ..strokeStyle = lingrad
         ..lineWidth = 1
